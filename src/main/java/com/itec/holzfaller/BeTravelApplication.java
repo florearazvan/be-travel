aab2753cfc1fc37d0fd3e18bb22ff2f85c1e9b28
Razvan Florea <razvan.florea@dialogdata.de>
2017-04-07 16:34:43 +0300

    login screen + FX app

--- src/main/java/com/itec/holzfaller/BeTravelApplication.java
+++ src/main/java/com/itec/holzfaller/BeTravelApplication.java
@@ -1,4 +1,28 @@
 package com.itec.holzfaller;
 
-public class BeTravelApplication {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class BeTravelApplication extends Application {
+
+    public static void main(String[] args) {
+        //TODO maybe here open another thread for the CLI
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Be-Travel");
+
+        URL url = getClass().getClassLoader().getResource("ui/login.fxml");
+        Parent root = FXMLLoader.load(url);
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 }
