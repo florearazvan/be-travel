6940f81d48952fa502114e614301678a2d9ce1ef
Razvan Florea <razvan.florea@dialogdata.de>
2017-04-07 21:02:12 +0300

    added users table

---
+++ src/main/java/com/itec/holzfaller/controllers/UsersController.java
@@ -0,0 +1,53 @@
+package com.itec.holzfaller.controllers;
+
+import com.itec.holzfaller.entities.User;
+import com.itec.holzfaller.services.UserService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+public class UsersController {
+
+    private UserService userService = new UserService();
+
+    @FXML
+    public TableView<User> usersTable;
+
+    public void initialize() {
+        initializeTableData();
+        initializeTableEvents();
+    }
+
+    private void initializeTableEvents() {
+        //double click event to edit user
+        usersTable.setRowFactory(table -> {
+            TableRow<User> row = new TableRow<>();
+
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2) {
+                    User clickedUser = row.getItem();
+                    System.out.println(clickedUser.getUsername());
+                }
+            });
+
+            return row;
+        });
+    }
+
+    private void initializeTableData() {
+        List<User> allUsers = userService.findAll();
+
+        for (TableColumn column : usersTable.getColumns()) {
+            column.setCellValueFactory(new PropertyValueFactory<User, StringJoiner>(column.getId()));
+        }
+        ObservableList<User> data = FXCollections.observableArrayList(allUsers);
+        usersTable.setItems(data);
+    }
+}
---
+++ src/main/java/com/itec/holzfaller/services/UserService.java
@@ -0,0 +1,16 @@
+package com.itec.holzfaller.services;
+
+import com.itec.holzfaller.entities.User;
+import com.itec.holzfaller.repository.UserRepo;
+
+import java.util.List;
+
+public class UserService {
+
+    private UserRepo userRepo = new UserRepo();
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+}
